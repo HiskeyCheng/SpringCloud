2ad338b7393a67b88fc187d879472a922aa1e3ef
cheng_zx <cheng_zx@suixingpay.com>
2018-01-30 20:49:37 +0800

    test

---
+++ springcloud02/suixingpay-Eureka/eureka-client/src/main/java/start/test/CallableThreadTest.java
@@ -0,0 +1,51 @@
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: 程志祥<cheng_zx@suixingpay.com>
+ * @date: 2018/1/29
+ * @Copyright: ©2017 Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+package start.test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @description:
+ * @author: 程志祥<cheng_zx@suixingpay.com>
+ * @date: 2018/1/29 上午11:43
+ * @version: V1.0
+ */
+
+public class CallableThreadTest implements Callable<Integer> {
+
+    public static void main(String[] args) {
+        CallableThreadTest ctt = new CallableThreadTest();
+        FutureTask<Integer> ft = new FutureTask<>(ctt);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " 的循环变量i的值" + i);
+            if (i == 20) {
+                new Thread(ft, "有返回值的线程").start();
+            }
+        }
+        /*try {
+            System.out.println("子线程的返回值：" + ft.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }*/
+
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int i = 0;
+        for (; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " call" + i);
+        }
+        return i;
+    }
+}
